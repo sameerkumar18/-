4c4cdae048172d3cc2f9b9f4544244466974648c
Sameer Kumar <sameer18051998@gmail.com>
2017-05-07 07:33:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/')
 def index():
     return render_template("index.html")
 
